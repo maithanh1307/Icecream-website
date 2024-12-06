775a5a9049dd8051aa0d5f1547c775e328acd372
maithanh1307 <nguyentranmaithanh1307@gmail.com>
2024-12-06 15:22:57 +0700

    complete manage coupon admin

--- backend/controllers/adminCoupon.js
+++ backend/controllers/adminCoupon.js
@@ -2,30 +2,57 @@ const express = require('express');
 const db = require('../models/index.js'); 
 var router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('AdminCoupon')
-})
-
-router.post('/addCoupons', async (req, res) => {
-    const { code, discount_value, discount_type, min_order_value, expiration_date, usage_limit } = req.body;
+router.get('/', async (req, res) => {  
     try {
-        const [result] = await db.promise().query(
-            `INSERT INTO coupons (code, discount_value, discount_type, min_order_value, expiration_date, usage_limit) 
-            VALUES (?, ?, ?, ?, ?, ?)`,
-            [code, discount_value, discount_type, min_order_value, expiration_date, usage_limit]
+        const [coupon] = await db.promise().query(
+            `SELECT 
+            coupon_id, 
+            code, 
+            discount_value, 
+            discount_type, 
+            DATE(created_at) AS create_at, 
+            DATE(expiration_date) AS expiration_date
+            FROM coupons ORDER BY coupon_id DESC;`
         );
-        res.status(201).json({
-            status: 'success',
-            message: 'Coupon created successfully!',
-        });
+        res.render('AdminCoupon', {coupon})
     } 
     catch (error) {
-        console.error('Error creating coupon:', error);
+        console.error('Error fetching coupons:', error);
         res.status(500).json({
+            status: 'error',
+            message: 'Error fetching coupons.',
+        });
+    }
+})
+
+router.post('/addCoupon', async (req, res) => {
+    const { code, discount_value, discount_type, expiration_date } = req.body;
+    try {
+        const [result] = await db.promise().query(
+            `INSERT INTO coupons (code, discount_value, discount_type, expiration_date) 
+            VALUES (?, ?, ?, ?)`,
+            [code, discount_value, discount_type, expiration_date]
+        );
+
+        // Định dạng lại giá trị discount_value để hiển thị
+        let formattedDiscountValue;
+        if (discount_type === 'percentage') {
+            formattedDiscountValue = `${discount_value}%`; // Dạng phần trăm
+        } 
+        else if (discount_type === 'fixed') {
+            formattedDiscountValue = `${Number(discount_value).toLocaleString()}đ`; // Dạng cố định
+        }
+
+        console.log("Coupon created: ", result);
+        res.redirect('/admin/manageCoupon');
+    } catch (error) {
+        console.error('Error creating coupon:', error);
+        res.status(500).send({
             status: 'error',
             message: 'Error creating coupon.',
         });
     }
 });
+
 
 module.exports = router;
